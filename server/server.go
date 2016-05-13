b30b3675c2e99f9c2dc7f182845d0a5f9dc4761b
Rich Beaudoin <rich.beaudoin.jr@gmail.com>
2016-05-12 23:56:53 -0600

    add zookeeper go client and test connection

--- server/server.go
+++ server/server.go
@@ -1,8 +1,10 @@
 package server
 
 import (
+	"github.com/samuel/go-zookeeper/zk"
 	"log"
 	"os"
+	"time"
 )
 
 func init() {
@@ -11,5 +13,14 @@ func init() {
 
 func Run(ip int, host string) {
 	log.Print("Initializing lamport...")
+	c, _, err := zk.Connect([]string{"127.0.0.1"}, time.Second)
+	if err != nil {
+		panic(err)
+	}
+	children, stat, _, err := c.ChildrenW("/")
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("Connected to Zookeper: %+v %+v\n", children, stat)
 	log.Printf("Running lamport on host: %s, port: %d", host, ip)
 }
