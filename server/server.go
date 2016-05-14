8f7760400067d96e5b4e8371c6ced9e38e63f18f
Rich Beaudoin <rich.beaudoin.jr@gmail.com>
2016-05-13 23:33:27 -0600

    move zookeeper connection into function

--- server/server.go
+++ server/server.go
@@ -13,6 +13,12 @@ func init() {
 
 func Run(ip int, host string) {
 	log.Print("Initializing lamport...")
+	connectZk()
+	log.Printf("Running lamport on host: %s, port: %d", host, ip)
+}
+
+func connectZk() {
+	log.Print("Connecting to Zookeper...")
 	c, _, err := zk.Connect([]string{"127.0.0.1"}, time.Second)
 	if err != nil {
 		panic(err)
@@ -22,5 +28,4 @@ func Run(ip int, host string) {
 		panic(err)
 	}
 	log.Printf("Connected to Zookeper: %+v %+v\n", children, stat)
-	log.Printf("Running lamport on host: %s, port: %d", host, ip)
 }
