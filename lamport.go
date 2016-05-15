43f53deeeab9fdd0fbdda29c2948b6777ded2b83
Rich Beaudoin <rich.beaudoin.jr@gmail.com>
2016-05-15 17:54:44 -0600

    setup lamport to listen on tcp scoket for connections

--- lamport.go
+++ lamport.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	ip := flag.Int("port", 5936, "The port on which lamport will listen for incoming connections")
+	ip := flag.String("port", "5936", "The port on which lamport will listen for incoming connections")
 	host := flag.String("host", "127.0.0.1", "The host ip on which lamport will run")
 
 	flag.Parse()
--- server/server.go
+++ server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"github.com/samuel/go-zookeeper/zk"
 	"log"
+	"net"
 	"os"
 	"time"
 )
@@ -11,21 +12,36 @@ func init() {
 	log.SetOutput(os.Stdout)
 }
 
-func Run(ip int, host string) {
+func Run(ip string, host string) {
 	log.Print("Initializing lamport...")
 	connectZk()
-	log.Printf("Running lamport on host: %s, port: %d", host, ip)
+	listen(ip, host)
+}
+
+func listen(ip string, host string) {
+	ln, err := net.Listen("tcp", host+":"+ip)
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("Lamport listening on " + host + ":" + ip)
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	log.Print("Incoming connection made...")
 }
 
 func connectZk() {
 	log.Print("Connecting to Zookeper...")
-	c, _, err := zk.Connect([]string{"127.0.0.1"}, time.Second)
+	_, _, err := zk.Connect([]string{"127.0.0.1"}, time.Second)
 	if err != nil {
 		panic(err)
 	}
-	children, stat, _, err := c.ChildrenW("/")
-	if err != nil {
-		panic(err)
-	}
-	log.Printf("Connected to Zookeper: %+v %+v\n", children, stat)
+	log.Printf("Connected to Zookeper")
 }
