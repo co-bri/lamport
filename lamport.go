db5cd448aa67b68223c1faf2c6d149b7c326ece5
Bo Ingram <boingram6@gmail.com>
2016-05-28 22:04:50 -0600

    Enable a single lamport node to become a leader using the raft protocol

--- lamport.go
+++ lamport.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/Distributed-Computing-Denver/lamport/server"
 )
 
 func main() {
 	ip := flag.String("port", "5936", "The port on which lamport will listen for incoming connections")
 	host := flag.String("host", "127.0.0.1", "The host ip on which lamport will run")
-
 	raftDir := flag.String("raftDir", ".raft", "The directory used for Raft storage")
-	raftNode := server.NewRaftNode(*raftDir)
+	raftIp := flag.String("raftPort", "8500", "The port on which the raft protocol will communicate")
+
+	raftNode, err := server.NewRaftNode(*raftIp, *host, *raftDir)
+	if err != nil {
+		panic(fmt.Errorf("Error creating raft node: %s", err))
+	}
 	flag.Parse()
 	server.Run(*ip, *host, raftNode)
 }
---
+++ server/raft_node.go
@@ -0,0 +1,85 @@
+package server
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/hashicorp/raft"
+	"github.com/hashicorp/raft-boltdb"
+)
+
+const (
+	maxTcpPoolSize      = 3
+	raftDbName          = "raft.db"
+	retainSnapshotCount = 2
+	tcpTimeout          = 10 * time.Second
+)
+
+func init() {
+	log.SetOutput(os.Stdout)
+}
+
+type RaftNode struct {
+	raft     *raft.Raft
+	raftAddr string
+	raftDir  string
+}
+
+func NewRaftNode(ip, port, raftDir string) (*RaftNode, error) {
+	r := &RaftNode{}
+
+	err := r.init(ip, port, raftDir)
+	if err != nil {
+		return nil, err
+	}
+
+	return r, nil
+}
+
+func (r *RaftNode) init(ip, host, raftDir string) error {
+	r.raftDir = raftDir
+	log.Printf("Setting raft directory to " + r.raftDir)
+
+	r.raftAddr = net.JoinHostPort(host, ip)
+	log.Printf("Setting raft address to " + r.raftAddr)
+
+	config := raft.DefaultConfig()
+	config.EnableSingleNode = true
+	config.DisableBootstrapAfterElect = false
+
+	addr, err := net.ResolveTCPAddr("tcp", r.raftAddr)
+	if err != nil {
+		return fmt.Errorf("Error resolving tcp address: %s", err)
+	}
+
+	transport, err :=
+		raft.NewTCPTransport(r.raftAddr, addr, maxTcpPoolSize, tcpTimeout, os.Stderr)
+	if err != nil {
+		return fmt.Errorf("Error creating raft TCP transport: %s", err)
+	}
+
+	peerStore := raft.NewJSONPeers(r.raftDir, transport)
+
+	snapshots, err := raft.NewFileSnapshotStore(r.raftDir, retainSnapshotCount, os.Stderr)
+	if err != nil {
+		return fmt.Errorf("Error creating file snap shot store: %s", err)
+	}
+
+	boltStore, err := raftboltdb.NewBoltStore(filepath.Join(r.raftDir, raftDbName))
+	if err != nil {
+		return fmt.Errorf("Error creating bolt db store: %s", err)
+	}
+
+	raft, err :=
+		raft.NewRaft(config, nil, boltStore, boltStore, snapshots, peerStore, transport)
+	if err != nil {
+		return fmt.Errorf("Error creating raft struct: %s", err)
+	}
+	r.raft = raft
+
+	return nil
+}
