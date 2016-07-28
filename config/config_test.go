bbb784b214aea82d78d7cac3cbf2b00aa9e65366
Rich Beaudoin <rich.beaudoin.jr@gmail.com>
2016-07-27 21:07:17 -0600

    fix config test issue

--- config/config_test.go
+++ config/config_test.go
@@ -11,37 +11,16 @@ func TestReadConfig(t *testing.T) {
 		t.Fatalf("Encountered error reading config file %s", testFile)
 	}
 
-	if c.Bootstrap != "127.0.0.1:5936" {
-		t.Fatalf("Expected 127.0.0.1:5936 for 'Bootstrap', found %s", c.Bootstrap)
-	}
-	if len(c.Zookeeper) != 3 {
-		t.Fatalf("Expected 3 items in 'Zookeeper', found %d", len(c.Zookeeper))
-	}
-	if c.Zookeeper[0] != "127.0.0.1:2190" {
-		t.Fatalf("Expected 127.0.0.1:2190 for 'Zookeeper[0]', found %s", c.Zookeeper[0])
-	}
-	if c.Zookeeper[1] != "127.0.0.2:2190" {
-		t.Fatalf("Expected 127.0.0.2:2190 for 'Zookeeper[1]', found %s", c.Zookeeper[1])
-	}
-	if c.Zookeeper[2] != "127.0.0.3:2190" {
-		t.Fatalf("Expected 127.0.0.3:2190 for 'Zookeeper[2]', found %s", c.Zookeeper[2])
-	}
 	if c.Host != "127.0.0.1" {
 		t.Fatalf("Expected 127.0.0.1 for 'IP', found %s", c.Host)
 	}
 	if c.Port != "5936" {
 		t.Fatalf("Expected 5936 for 'Port', found %s", c.Port)
 	}
-	if c.RaftDir != ".raft" {
-		t.Fatalf("Expected .raft for 'RaftDir', found %s", c.RaftDir)
-	}
-	if c.RaftPort != "8500" {
-		t.Fatalf("Expected 8500 for 'Bootstrap', found %s", c.RaftPort)
-	}
 }
 
 func TestReadConfigError(t *testing.T) {
-	c, err := ReadConfig("foo.toml")
+	_, err := ReadConfig("foo.toml")
 	if err == nil {
 		t.Fatal("Expected error when reading non-existent config")
 	}
